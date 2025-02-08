1c62f28cc7ccb48431d81b09f0a9fac7ec5c3656
mcbunkus <mcbunkus@localhost.localdomain>
2025-02-08 17:00:46 -0500

    removed all runtime path dependencies

---
+++ src/Fonts/Font.h
@@ -0,0 +1,10 @@
+#ifndef _GAMB_FONTS_FONT_H
+#define _GAMB_FONTS_FONT_H
+
+typedef struct
+{
+    unsigned int Len;
+    unsigned char Data[];
+} Font;
+
+#endif // !_GAMB_FONTS_FONT_H
---
+++ src/Fonts/Square.h
@@ -0,0 +1,8 @@
+#ifndef _GAMB_FONTS_SQUARE_H
+#define _GAMB_FONTS_SQUARE_H
+
+#include "Font.h"
+
+extern Font SquareFont;
+
+#endif // !_GAMB_FONTS_SQUARE_H
